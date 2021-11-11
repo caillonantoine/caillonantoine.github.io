c66aa035ae24662f41d4987bed78b54d0751e528
Antoine <caillonantoine@gmail.com>
2021-11-11 11:36:11 +0100

    change dsynth

--- assets/install_dsynth.sh
+++ assets/install_dsynth.sh
@@ -1,13 +1,13 @@
 set +e
-LIBTORCH="https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.9.0.zip"
+LIBTORCH="https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.10.0.zip"
 
 # GET LIBTORCH
 if [ ! -d "/usr/local/share/libtorch/" ]
 then
 echo "downloading libtorch"
-curl $LIBTORCH -o libtorch.zip &> /dev/null
+curl $LIBTORCH -o libtorch.zip 
 echo "unzipping libtorch"
-unzip libtorch.zip &> /dev/null
+unzip libtorch.zip 
 rm libtorch.zip
 echo "installing libtorch"
 mv libtorch /usr/local/share/
@@ -19,6 +19,6 @@ fi
 #GET DSYNTH
 echo "downloading dsynth"
 DSYNTH="https://raw.githubusercontent.com/caillonantoine/caillonantoine.github.io/master/assets/dsynth~.zip"
-curl $DSYNTH -o dsynth.zip &> /dev/null
-unzip dsynth.zip &> /dev/null
-rm dsynth.zip &> /dev/null
+curl $DSYNTH -o dsynth.zip 
+unzip dsynth.zip 
+rm dsynth.zip 
